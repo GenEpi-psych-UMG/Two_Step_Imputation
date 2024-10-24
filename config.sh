1cfa4224272d729a17961c8acac178caf42f386e
MkamalNasr <mohammed.nasr@uni-greifswald.de>
2024-10-24 15:50:07 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # common files, tools and parameters to all imputation scripts
-WD=/daten/gtnas/users/nasrk/Imputation/Scripts/Project
+WD=/path/to/Project
 Bcftools=bcftools
 Tabix=tabix
 minimac4=minimac4
-cohorts=/daten/gtnas/users/nasrk/Imputation/Scripts/Project/Cohorts_Info.csv
-fasta=/daten/gtnas/users/nasrk/Imputation/Data/human_g1k_v37.fasta
-eagle=/daten/gtnas/users/nasrk/Imputation/Programs/Eagle_v2.4.1/eagle
-gmap=/daten/gtnas/users/nasrk/Imputation/Programs/Eagle_v2.4.1/tables/genetic_map_hg19_withX.txt.gz
+cohorts=/path/to/Project/Cohorts_Info.csv
+fasta=/path/to/human_g1k_v37.fasta
+eagle=/path/to/Eagle_v2.4.1/eagle
+gmap=/path/to/genetic_map_hg19_withX.txt.gz
 cpus=12
 rounds=5
-INFO=/daten/gtnas/users/nasrk/Imputation/Scripts/Project/extract_info_VCF.pl
+INFO=/path/to/Project/extract_info_VCF.pl
 VCFtools=vcftools
 
 
