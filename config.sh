c9b3e650a68b418d30bb59d3df901128bfe603be
MkamalNasr <mohammed.nasr@uni-greifswald.de>
2024-10-24 16:54:01 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,13 +5,13 @@ WD=/path/to/Project
 Bcftools=bcftools
 Tabix=tabix
 minimac4=minimac4
-cohorts=/path/to/Project/Cohorts_Info.csv
+cohorts="${WD}/Cohorts_Info.csv"
 fasta=/path/to/human_g1k_v37.fasta
 eagle=/path/to/Eagle_v2.4.1/eagle
 gmap=/path/to/genetic_map_hg19_withX.txt.gz
 cpus=12
 rounds=5
-INFO=/path/to/Project/extract_info_VCF.pl
+INFO="${WD}/extract_info_VCF.pl"
 VCFtools=vcftools
 
 
